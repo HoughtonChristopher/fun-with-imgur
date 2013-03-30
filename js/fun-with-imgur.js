fb10c4f03274581af78cab113a9224fa0e0dc817
Nathaniel Williams <eruonen@gmail.com>
2013-03-30 01:06:43 +0100

    Update fun-with-imgur.js

--- js/fun-with-imgur.js
+++ js/fun-with-imgur.js
@@ -1,1 +1,36 @@
-$(document).ready(function(){function e(){var e=$("#word").val().toLowerCase().split("");var t=Math.pow(2,e.length);$("#result").html("");var n="";for(var r=0;r<t;r++){var i=new Array;for(var s=0;s<e.length;s++){if(/^[a-zA-Z]*$/.test(e[s]))i[s]=r>>s&1?e[s].toUpperCase():e[s];else i[s]=e[s]}$("#result").append('<a title="'+i.join("")+'" class="imgur_result" target="_blank" href="http://imgur.com/'+i.join("")+'"><img class="imgur_prev" src="http://i.imgur.com/'+i.join("")+'s.jpg"></a>')}}$("#submit").click(function(){if($("#word").val().length<5){alert("You need to enter five characters!")}else{e()}});$("#word").keypress(function(t){if(t.which==13){if($("#word").val().length<5){alert("You need to enter five characters!")}else{e()}}})})
+jQuery(document).ready(function (#) {
+  function get_permutation() {
+    var characters = $("#word").val().toLowerCase().split("");
+    var max = Math.pow(2, characters.length);
+    $("#result").html("");
+    var prev = "";
+    for (var i = 0; i < max; i++) {
+        var permutation = new Array();
+        for (var j = 0; j < characters.length; j++) {
+          if (/^[a-zA-Z]*$/.test(characters[j]))
+            permutation[j] = i >> j & 1 ? characters[j].toUpperCase() : characters[j];
+          else
+            permutation[j] = characters[j];
+        }
+          $("#result").append('<a title="'
+            + permutation.join("")
+            + '" class="imgur_result" target="_blank" href="http://imgur.com/'
+            + permutation.join("") 
+            + '"><img class="imgur_prev" src="http://i.imgur.com/' 
+            + permutation.join("") + 's.jpg"></a>');
+    }
+  }
+  $("#submit").click(function () {
+    do_action();
+  });
+  $("#word").keypress(function (e) {
+    if (e.which == 13)
+      do_action();
+  });
+  function do_action() {
+    if ($("#word").val().length < 5)
+      alert("You need to enter five characters!");
+    else
+      get_permutation();
+  }
+});
